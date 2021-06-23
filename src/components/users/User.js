e2d4fd0300d1cec209b5647dbc6fff0ddc5affb4
novik <annie5donchenko@gmail.com>
2021-06-23 17:21:47 +0300

    adding additional task

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,11 +1,20 @@
 
 import PostsForUser from "../postsForUser/PostsForUser";
+import {useState} from "react";
 
 export default function User ({item:{id, name, username, email, address, phone, website, company}}){
+
+    // let hideBtnItem = document.getElementById('hide-posts-btn');
+    // let [hideBtn, setHideBtn] = useState(hideBtnItem);
+
+    // console.log(hideBtn);
+    // setHideBtn();
+
     return (
         <div>
             <h2>{name} <i>{username}</i></h2>
-            <div className="posts">
+            <button id={'hide-posts-btn'}>Hide All Posts</button>
+            <div className="posts active">
                 <PostsForUser id={id}/>
             </div>
             <hr/>
