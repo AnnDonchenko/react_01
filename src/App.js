d2df7ea7ba2feffc8ed71248cc2b143d1ec7bb8c
novik <annie5donchenko@gmail.com>
2021-06-24 19:29:16 +0300

    working with lesson_02

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import {useEffect, useState} from 'react';
+import {getUsers, getPosts} from './connection/API';
+import Users from './components/users/Users';
 
 function App() {
+
+    const [users, setUsers] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const [userPosts, setUserPosts] = useState([]);
+
+    useEffect( () => {
+       getUsers().then(value => setUsers([...value.data]))
+    },[]);
+
+    useEffect( () => {
+        getPosts().then(value => setPosts([...value.data]))
+    },[]);
+
+    const showPosts = (id) => {
+        getPosts().then(value => setPosts([...value.data]));
+        let filter = posts.filter(value => value.userId === id);
+        setUserPosts(filter);
+    }
+
+
   return (
-    <div>
-        <h1>This master branch</h1>
+    <div className={'wrap'}>
+        <h1>This lesson_02 branch</h1>
+        <Users items={users} showPosts={showPosts} userPosts={userPosts}/>
     </div>
   );
 }
