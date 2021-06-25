e05b6645f07be471ee38797442e751379d9df7f4
novik <annie5donchenko@gmail.com>
2021-06-25 13:24:11 +0300

    working with lesson_02

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {useEffect, useState} from 'react';
-import {getUsers, getPosts} from './connection/API';
+import {getUsers, getPosts, getComments} from './connection/API';
 import Users from './components/users/Users';
+import Posts from './components/posts/Posts';
 
 function App() {
 
     const [users, setUsers] = useState([]);
     const [posts, setPosts] = useState([]);
-    const [userPosts, setUserPosts] = useState([]);
+    const [comments, setComments] = useState([]);
 
     useEffect( () => {
        getUsers().then(value => setUsers([...value.data]))
@@ -17,17 +18,26 @@ function App() {
         getPosts().then(value => setPosts([...value.data]))
     },[]);
 
+    useEffect(()=>{
+        getComments().then(value=> setComments([...value.data]))
+    },[])
+
     const showPosts = (id) => {
         getPosts().then(value => setPosts([...value.data]));
         let filter = posts.filter(value => value.userId === id);
-        setUserPosts(filter);
+        return filter;
     }
 
+    const showComments = (id) =>{
+        getComments().then(value => setComments([...value.data]));
+        let filter = comments.filter(value => value.postId === id);
+        return filter;
+    }
 
   return (
     <div className={'wrap'}>
         <h1>This lesson_02 branch</h1>
-        <Users items={users} showPosts={showPosts} userPosts={userPosts}/>
+        <Users items={users} showPosts={showPosts} showComments={showComments}/>
     </div>
   );
 }
