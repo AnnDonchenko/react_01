7740b90eb0b43bbd3ce89afea5dfa6313cb1988e
novik <annie5donchenko@gmail.com>
2021-06-29 19:35:02 +0300

    Reducer

--- src/App.js
+++ src/App.js
@@ -9,9 +9,28 @@
 import './App.css';
 import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom";
 import Users from "./components/users/Users";
+import {useReducer} from "react";
+
+const INC_COUNTER1 = 'INC_COUNTER1';
+
+const reducer = (state, action) => {
+    console.log(action);
+    switch (action.type) {
+        case INC_COUNTER1:
+            return {...state, counter1: state.counter1 + action.payload}
+        default:
+            return state
+    }
+}
 
 function App() {
-  return (
+    const initialState = {
+        counter1: 0,
+        counter2: 0,
+    }
+
+    const [{counter1, counter2}, dispatch] = useReducer(reducer, initialState);
+    return (
     <div>
         <h1>This is lesson_03 branch</h1>
         <Router>
@@ -24,7 +43,12 @@ function App() {
                 <Route path={'/users'} componet={Users}/>
             </Switch>
         </Router>
-
+        <hr/>
+        <h2>Counter 1: {counter1}</h2>
+        <button onClick={() => dispatch({type: INC_COUNTER1, payload: 5})}>Inc 1</button>
+        <hr/>
+        <h2>Counter 2: {counter2}</h2>
+        <button>Inc 2</button>
 
     </div>
   );
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,16 +1,13 @@
-import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom";
 import PostsForUser from "../posts/PostsForUser";
 import './Users.css';
 
 export default function User ({user}){
     return (
-        <Router>
+        <div>
             <p className={'user-name'}>{user.name}</p>
-            <Link to={'/users/' + user.id + '/posts'}>Show posts</Link>
+            {/*<button onClick={()=>{}}>Show posts</button>*/}
 
-            <Switch>
-                <Route path={'/users/' + user.id + '/posts'} render={()=><PostsForUser userID={user.id}/>}/>
-            </Switch>
-        </Router>
+            <PostsForUser userID={user.id}/>}/>
+        </div>
     );
 }
